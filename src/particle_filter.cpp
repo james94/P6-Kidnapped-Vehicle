091d86e8d2dc02bc3517442b902bcd2a8b3d7fab
james94 <11840449+james94@users.noreply.github.com>
2019-12-02 20:47:16 -0800

    Implemented Particle Filter init()
    Initializes Particle Filter by initializing all the filter's
    particles and adding random Gaussian noise to each particle's
    x, y and theta using random gen. The x and y are the initial
    gps position estimate and theta is the initial heading estimate.

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -3,6 +3,8 @@
  *
  * Created on: Dec 12, 2016
  * Author: Tiffany Huang
+ * Updated on: Dec 2, 2019
+ * Contributor: James Medel
  */
 
 #include "particle_filter.h"
@@ -20,18 +22,43 @@
 
 using std::string;
 using std::vector;
+using std::normal_distribution;
+using std::default_random_engine;
 
+/**
+ * init() function
+ * 
+ * Initializes Particle Filter by initializing all the filter's particles and
+ * sampling from a gaussian distribution taking into account gaussian sensor
+ * noise around the initial gps position estimate and initial heading estimate.
+ */
 void ParticleFilter::init(double x, double y, double theta, double std[]) {
-  /**
-   * TODO: Set the number of particles. Initialize all particles to 
-   *   first position (based on estimates of x, y, theta and their uncertainties
-   *   from GPS) and all weights to 1. 
-   * TODO: Add random Gaussian noise to each particle.
-   * NOTE: Consult particle_filter.h for more information about this method 
-   *   (and others in this file).
-   */
-  num_particles = 0;  // TODO: Set the number of particles
+  default_random_engine gen;
+  // Set the number of particles
+  num_particles = 10;
 
+  // Create normal (Gaussian) distribution for particle x, y, theta
+  normal_distribution<double> dist_particle_x(x, std[0]);
+  normal_distribution<double> dist_particle_y(y, std[1]);
+  normal_distribution<double> dist_particle_theta(theta, std[2]);
+
+  // Initialize all particles to first position and all weights to 1
+  Particle particle;
+  particle.weight = 1;
+
+  for(int i = 0; i < num_particles; ++i)
+  {
+    particle.id = i;
+    // Add random Gaussian noise to each particle's x,y,theta using gen
+    particle.x = dist_particle_x(gen);
+    particle.y = dist_particle_y(gen);
+    particle.theta = dist_particle_theta(gen);
+    particles.push_back(particle);
+    weights.push_back(particle.weight);
+  }
+
+  // particle filter is initialized
+  is_initialized = true;
 }
 
 void ParticleFilter::prediction(double delta_t, double std_pos[], 
