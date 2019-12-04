e457cd15b8a42dba18667fc8158a6c206a1c9ca2
james94 <11840449+james94@users.noreply.github.com>
2019-12-03 21:52:53 -0800

    Added multivariate gaussian equation
    From Udacity Term 2 Lesson 5, I referenced the
    code for Multi-variate Gaussian Distribution
    equation and incorporated it into my project.
    So, I can calculate the weight for transformed
    observation and map landmark point for current
    particle. The final particle weight is found
    by multiplying all the weights across all
    transformed observations and map landmarks.
    This function was easy to understand and already
    had clean code and super useful.

--- src/helper_functions.h
+++ src/helper_functions.h
@@ -49,6 +49,29 @@ struct LandmarkObs {
 };
 
 /**
+ * Computes Multi-variate Gaussian Distribution
+ * 
+ * Referenced Particle Weights Solution from Udacity SDCE-ND T2L5
+ */
+inline double multiv_prob(double sig_x, double sig_y, double x_tobs, double y_tobs,
+                          double mu_x, double mu_y) {
+  // calculate normalization term
+  double gauss_norm;
+  gauss_norm = 1 / (2 * M_PI * sig_x * sig_y);
+
+  // calculate exponent
+  double exponent;
+  exponent = ( pow(x_tobs - mu_x, 2) / (2 * pow(sig_x, 2)) ) +
+             ( pow(y_tobs - mu_y, 2) / (2 * pow(sig_y, 2)) );
+
+  // calculate weight using normalization terms and exponent
+  double weight;
+  weight = gauss_norm * exp(-exponent);
+  
+  return weight;
+}
+
+/**
  * Computes the Euclidean distance between two 2D points.
  * @param (x1,y1) x and y coordinates of first point
  * @param (x2,y2) x and y coordinates of second point
